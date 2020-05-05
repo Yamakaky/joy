37027105d183f3f28094ac7b2ccdef8400baac66
Yamakaky <yamakaky@yamaworld.fr>
2020-05-05 12:15:56 +0200

    Only used there

--- joycon-sys/src/common.rs
+++ joycon-sys/src/common.rs
@@ -1,7 +1,3 @@
-use num::{FromPrimitive, ToPrimitive};
-use std::fmt;
-use std::marker::PhantomData;
-
 pub const NINTENDO_VENDOR_ID: u16 = 1406;
 
 pub const JOYCON_L_BT: u16 = 0x2006;
@@ -19,32 +15,4 @@ pub enum SubcommandId {
     SetInputReportMode = 0x03,
     SetPlayerLights = 0x30,
     EnableIMU = 0x40,
-}
-
-#[repr(C)]
-#[derive(Copy, Clone)]
-pub struct RawId<Id>(pub u8, PhantomData<Id>);
-
-impl<Id: FromPrimitive> RawId<Id> {
-    pub fn try_into(self) -> Option<Id> {
-        Id::from_u8(self.0)
-    }
-}
-
-impl<Id: ToPrimitive> From<Id> for RawId<Id> {
-    fn from(id: Id) -> Self {
-        RawId(id.to_u8().expect("always one byte"), PhantomData)
-    }
-}
-
-impl<Id: fmt::Debug + FromPrimitive + Copy> fmt::Debug for RawId<Id> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        if let Some(id) = self.try_into() {
-            write!(f, "{:?}", id)
-        } else {
-            f.debug_tuple("RawId")
-                .field(&format!("{:x}", self.0))
-                .finish()
-        }
-    }
 }
