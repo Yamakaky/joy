5318515570623e1bbb6a0d4ec831ae3b42991dbe
Yamakaky <yamakaky@yamaworld.fr>
2020-05-07 19:33:26 +0200

    Write image trace

--- src/main.rs
+++ src/main.rs
@@ -25,8 +25,16 @@ fn main() -> anyhow::Result<()> {
         dbg!(device.load_calibration()?);
         device.reset_calibration()?;
 
+        let iter_count = 600;
         let mut rotation = Vector3::default();
-        for i in 0..600 {
+        let mut image = image::ImageBuffer::new(iter_count, 128);
+        for i in 0..iter_count {
+            for y in 0..(device.max_raw_gyro / 256) {
+                image.put_pixel(i, 127 - y as u32, image::Luma([128u8]));
+            }
+            device.max_raw_gyro = 0;
+            image.put_pixel(i, 127 - 32, image::Luma([255u8]));
+            image.put_pixel(i, 127 - 64, image::Luma([255u8]));
             for delta in &device.get_gyro_rot_delta(false)? {
                 rotation += *delta;
             }
@@ -36,8 +44,7 @@ fn main() -> anyhow::Result<()> {
                 println!("{:?}", device.get_accel_delta_g(false)?[0]);
             }
         }
-        println!("{:?}", device.max_raw_gyro);
-        println!("{:?}", device.max_raw_accel);
+        image.save("D:\\gyro.png")?;
     }
     Ok(())
 }
