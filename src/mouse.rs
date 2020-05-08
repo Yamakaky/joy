8fb46fc7f545b9bac2671125457c384ab40a6e73
Yamakaky <yamakaky@yamaworld.fr>
2020-05-08 10:33:16 +0200

    Add rolling mouse offset over frames

---
+++ src/mouse.rs
@@ -0,0 +1,20 @@
+use enigo::*;
+
+#[derive(Copy, Clone, Debug, Default)]
+pub struct Mouse {
+    diff_x: f32,
+    diff_y: f32,
+}
+
+impl Mouse {
+    pub fn move_relative(&mut self, enigo: &mut Enigo, mut x: f32, mut y: f32) {
+        x += self.diff_x;
+        y += self.diff_y;
+        let round_x = x.round();
+        let round_y = y.round();
+        self.diff_x = x - round_x;
+        self.diff_y = y - round_y;
+
+        enigo.mouse_move_relative(round_x as i32, round_y as i32);
+    }
+}
