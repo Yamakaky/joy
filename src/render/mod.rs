026c230f80262d58e95dbab852f629997c65e74e
Yamakaky <yamakaky@yamaworld.fr>
2020-06-03 20:07:27 +0200

    Enable back culling

--- src/render/mod.rs
+++ src/render/mod.rs
@@ -42,7 +42,7 @@ pub fn create_render_pipeline(
         }),
         rasterization_state: Some(wgpu::RasterizationStateDescriptor {
             front_face: wgpu::FrontFace::Ccw,
-            cull_mode: wgpu::CullMode::None,
+            cull_mode: wgpu::CullMode::Back,
             depth_bias: 0,
             depth_bias_slope_scale: 0.0,
             depth_bias_clamp: 0.0,
