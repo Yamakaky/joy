b745925aab7db1c605e8aec20c382be8dacff5b6
Yamakaky <yamakaky@yamaworld.fr>
2021-02-09 15:38:03 +0100

    Moar traces

--- joycon-sys/src/common.rs
+++ joycon-sys/src/common.rs
@@ -41,6 +41,17 @@ pub enum SubcommandId {
     SetIMUMode = 0x40,
     SetIMUSens = 0x41,
     EnableVibration = 0x48,
+
+    // arg [4,0,0,2], ret [0,8,0,0,0,0,0,44]
+    // arg [4,4,5,2], ret [0,8,0,0,0,0,200]
+    // arg [4,4,50,2], ret [0,8,0,0,0,0,5,0,0,14]
+    // arg [4,4,10,2], ret [0,20,0,0,0,0,244,22,0,0,230,5,0,0,243,11,0,0,234,12, 0, 0]
+    // get ringcon calibration: arg [4,4,26,2]
+    //                          ret [0,20,0,0,0,0] + [135, 8, 28, 0, 48, 247, 243, 0, 44, 12, 224]
+    // write ringcon calibration: arg [20,4,26,1,16] + [135, 8, 28, 0, 48, 247, 243, 0, 44, 12, 224]
+    //                            ret [0, 4]
+    // get number steps offline ringcon: arg [4,4,49,2], ret [0,8,0,0,0,0,nb_steps, 0,0, 127|143]
+    // reset number steps offline ringcon: arg [8,4,49,1,4], ret [0,4]
     Unknown0x58 = 0x58,
     // Always [] arg, [0, 32] return
     Unknown0x59 = 0x59,
