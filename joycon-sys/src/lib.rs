ecec2602d99075f5964fe0e4e1945dd7be470a50
Yamakaky <yamakaky@yamaworld.fr>
2021-02-13 10:52:40 +0100

    typo

--- joycon-sys/src/lib.rs
+++ joycon-sys/src/lib.rs
@@ -1,7 +1,7 @@
 //! Helper crate for interacting with a JoyCon and Switch Pro Controller via HID.
 //!
 //! The main structs are [InputReport](input/struct.InputReport.html) and
-//! [OutputReport](input/struct.OutputReport.html).
+//! [OutputReport](output/struct.OutputReport.html).
 
 #[macro_use]
 extern crate num_derive;
