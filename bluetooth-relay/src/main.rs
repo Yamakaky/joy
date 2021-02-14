b863483aca76b907da20c9200e0357462950033a
Yamakaky <yamakaky@yamaworld.fr>
2021-02-14 20:37:14 +0100

    Add bluetooth relay

--- bluetooth-relay/src/main.rs
+++ bluetooth-relay/src/main.rs
@@ -1,44 +1,115 @@
+use anyhow::Context;
 use bluetooth_sys::*;
-use std::{convert::TryInto, mem::zeroed};
+use hidapi::HidApi;
+use joycon::joycon_sys::{output::SubcommandRequest, OutputReport, NINTENDO_VENDOR_ID};
+use libc::sockaddr;
+use socket2::{SockAddr, Socket};
+use std::{
+    convert::TryInto,
+    ffi::CString,
+    mem::{size_of_val, zeroed},
+    thread::sleep,
+    time::{Duration, Instant},
+};
 
-fn main() {
+fn main() -> anyhow::Result<()> {
+    let api = HidApi::new()?;
+    let device_info = api
+        .device_list()
+        .find(|x| x.vendor_id() == NINTENDO_VENDOR_ID)
+        .unwrap();
+    let device = device_info.open_device(&api).context("open device")?;
+
+    dbg!("hid");
+
+    let (mut _client_ctl, mut client_itr) = connect_switch()?;
+
+    let subcmd = OutputReport::from(SubcommandRequest::request_device_info());
+    device.write(subcmd.as_bytes())?;
+
+    dbg!("raw");
+    sleep(Duration::from_millis(200));
+
+    let start = Instant::now();
+    loop {
+        {
+            let mut buf = [0; 500];
+            buf[0] = 0xa1;
+            let len = device
+                .read_timeout(&mut buf[1..], 0)
+                .context("joycon recv")?;
+            if len > 0 {
+                println!(
+                    "> {:.4} {}",
+                    start.elapsed().as_secs_f64(),
+                    hex::encode(&buf[1..len + 1])
+                );
+                if let Err(e) = client_itr.send(&buf[..len + 1]) {
+                    if e.raw_os_error() == Some(107) {
+                        eprintln!("Reconnecting the switch");
+                        let x = connect_switch()?;
+                        _client_ctl = x.0;
+                        client_itr = x.1;
+
+                        let subcmd = OutputReport::from(SubcommandRequest::request_device_info());
+                        device.write(subcmd.as_bytes())?;
+                    }
+                }
+            }
+        }
+        {
+            let mut buf = [0; 500];
+            if let Ok(len) = client_itr.recv(&mut buf).context("switch recv") {
+                if len > 0 {
+                    println!(
+                        "< {:.4} {}",
+                        start.elapsed().as_secs_f64(),
+                        hex::encode(&buf[1..len + 1])
+                    );
+                    device.write(&buf[1..len]).context("joycon send")?;
+                }
+            }
+        }
+        sleep(Duration::from_millis(1))
+    }
+}
+
+fn connect_switch() -> anyhow::Result<(Socket, Socket)> {
+    let client_ctl = Socket::new(
+        (AF_BLUETOOTH as i32).into(),
+        (__socket_type_SOCK_SEQPACKET as i32).into(),
+        Some((BTPROTO_L2CAP as i32).into()),
+    )?;
+    let client_itr = Socket::new(
+        (AF_BLUETOOTH as i32).into(),
+        (__socket_type_SOCK_SEQPACKET as i32).into(),
+        Some((BTPROTO_L2CAP as i32).into()),
+    )?;
+
     unsafe {
-        let client_ctl = socket(
-            AF_BLUETOOTH.try_into().unwrap(),
-            __socket_type_SOCK_SEQPACKET.try_into().unwrap(),
-            BTPROTO_L2CAP.try_into().unwrap(),
-        );
-        let client_itr = socket(
-            AF_BLUETOOTH.try_into().unwrap(),
-            __socket_type_SOCK_SEQPACKET.try_into().unwrap(),
-            BTPROTO_L2CAP.try_into().unwrap(),
-        );
         let mut addr = sockaddr_l2 {
             l2_family: AF_BLUETOOTH.try_into().unwrap(),
             // todo: watch out endian
-            l2_psm: 17,
+            l2_psm: 17u16.to_le(),
             ..zeroed()
         };
-        str2ba(
-            b"58:2F:40:DF:31:31\0".as_ptr() as *const i8,
-            &mut addr.l2_bdaddr,
+        let sa = CString::new("58:2F:40:DF:31:31")?;
+        str2ba(sa.as_ptr(), &mut addr.l2_bdaddr);
+        let ctl_addr = SockAddr::from_raw_parts(
+            &addr as *const _ as *const sockaddr,
+            size_of_val(&addr) as u32,
         );
-        assert_eq!(
-            0,
-            connect(
-                client_ctl,
-                &addr as *const _ as *const sockaddr,
-                std::mem::size_of_val(&addr).try_into().unwrap(),
-            )
+        client_ctl.connect(&ctl_addr)?;
+        client_ctl.set_nonblocking(true)?;
+
+        addr.l2_psm = 19u16.to_le();
+        let itr_addr = SockAddr::from_raw_parts(
+            &addr as *const _ as *const sockaddr,
+            size_of_val(&addr) as u32,
         );
-        addr.l2_psm = 19;
-        assert_eq!(
-            0,
-            connect(
-                client_itr,
-                &addr as *const _ as *const sockaddr,
-                std::mem::size_of_val(&addr).try_into().unwrap(),
-            )
-        );
+        client_itr.connect(&itr_addr)?;
+        client_itr.set_nonblocking(true)?;
     }
+
+    Ok((client_ctl, client_itr))
 }
